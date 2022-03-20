e7ebc0bb641ef61642fdd33c5cbea80dc4c98317
DamiAkinniyi <oluwadamilola.akinniyi@students.iaac.net>
2022-03-20 18:38:12 -0500

    Update sscript.js

--- src/examples/Portraits 3/sscript.js
+++ src/examples/Portraits 3/sscript.js
@@ -21,7 +21,7 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.m
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
 import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
 import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
-import { Line, LineCylinderIntersection, LineSphereIntersection } from 'rhino3dm'
+//import { Line, LineCylinderIntersection, LineSphereIntersection } from 'rhino3dm'
 
 // set up loader for converting the results to threejs
 const loader = new Rhino3dmLoader()
